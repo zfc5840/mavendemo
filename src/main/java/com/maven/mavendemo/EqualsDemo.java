deb2b90314fb707f4988d85043c66ca0db2b1608
zfc5840 <1076231986@qq.com>
2015-04-14 19:15:08 +0800

    添加equals与hashcode()方法测试，以及String的equals比较

---
+++ src/main/java/com/maven/mavendemo/EqualsDemo.java
@@ -0,0 +1,54 @@
+package com.maven.mavendemo;
+
+public class EqualsDemo {
+	private String name;
+	
+  
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public static void main(String[] args){
+		EqualsDemo equalsDemo = new EqualsDemo();
+		equalsDemo.setName("a");
+		EqualsDemo equalsDemo2 = new EqualsDemo();
+		equalsDemo2.setName("a");
+		System.out.println(equalsDemo.hashCode());
+		System.out.println(equalsDemo2.hashCode());
+		String dd="a";
+		String ddd="a";
+		if(dd.equals(ddd)){
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+		System.out.println("A".hashCode());
+		String c= new String("a");
+		String cc=new String("a");
+		System.out.println(c.hashCode());
+		System.out.println(cc.hashCode());
+		if(c.equals(cc)){
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+		if(equalsDemo==equalsDemo2){
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+		
+		if(equalsDemo.equals(equalsDemo2)){
+			System.out.println(true);
+		}else{
+			System.out.println(false);
+		}
+		
+	}
+}
